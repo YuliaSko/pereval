84ca01b5a57439105546ab81833d71e660f74456
yuliasko <skorobogatova.yulia@mail.ru>
2024-11-26 21:37:51 +0500

    add views, urls, yasg

--- app_pereval/views.py
+++ app_pereval/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import *
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class CoordViewSet(viewsets.ModelViewSet):
+    queryset = Coord.objects.all()
+    serializer_class = CoordSerializer
+
+
+class PerevalViewSet(viewsets.ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
+
+
+class PerevalImageViewSet(viewsets.ModelViewSet):
+    queryset = PerevalImage.objects.all()
+    serializer_class = PerevalImageSerializer
+
+
